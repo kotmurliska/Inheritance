2535ae4ea28c4505d05e838148e4be66ecfdfdd0
Vasiliy Cherniatyn <v.v.4ern9tin@gmail.com>
2016-08-19 21:12:19 +0300

    Initial commit

---
+++ Fan.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Fan
+    {
+        public Fan(DateTime birthdate, Address birthplace, params )
+            : base(birthdate, birthplace)
+        { }
+        public Band[] Groups { get; set; }
+        public void Shout()
+        { }
+    }
+}
